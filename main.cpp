1565c751d0183059c5bdd5d682ea09a9d7092ff0
Sohom829 <94981761+Sohom829@users.noreply.github.com>
2022-03-28 18:29:44 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//including the function
+#include "print.hpp"
+using namespace std;
+// main function
+int main() {
+  // printing using print function
+  print("hi");
+}
---
+++ print.hpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+//creating the function
+void print(string String) { // getting a string from user
+  // returing the string
+  cout<< String;
+}
